fbbfcc01fbf878995e90c21b3b2730d4a9188e8e
Billy Rodriguez <billyvecuss@hotmail.com>
2021-07-08 12:43:14 -0500

    Convert graphiQL to ApolloServer

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import compression from 'compression';
 import express from 'express';
 import cors from 'cors';
-import { graphqlHTTP } from 'express-graphql';
 import schema from "./graphql/schema"
+import { ApolloServer } from 'apollo-server-express';
+import { createServer } from 'http';
 const app = express();
 app.use(cors());
 app.use(compression());
 
-
-app.use("/", graphqlHTTP({
-    // schema: schema
+// Init ApolloServer using setting the schema
+const server = new ApolloServer({
     schema,
-    graphiql: true
-}))
+    introspection: true
+});
+
+server.applyMiddleware({app})
+
 // INITIALIZE APPLICATION
 const PORT = 3000
-
-app.listen({ port: PORT }, () => console.log(`Sever on port ${PORT}`))
+const httpServer = createServer(app)
+httpServer.listen({ port: PORT }, () => console.log(`Sever on port ${PORT}`))
